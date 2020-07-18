ad9e2974d7c32cb1582cb57812039eda8ad6e0b3
Dan Matthew <danielmatthew@users.noreply.github.com>
2020-07-18 18:02:38 +0100

    chore(tabs): add test coverage (#22)
    * chore(tabs): scaffold tab tests
    
    * chore(tabs): ignore coverage dir
    
    * refactor: move markup into variable
    
    * chore(ci): configure circle
    
    * chore(ci): update default image

---
+++ components/tabs/test/tabs.test.js
@@ -0,0 +1,53 @@
+import { fixture, html, expect, elementUpdated } from '@open-wc/testing';
+import '../awc-tabs.js';
+
+describe('awc-tabs', () => {
+  const markup = html`
+    <awc-tabs>
+      <awc-tab role="heading" slot="tab"></awc-tab>
+      <awc-panel role="region" slot="panel"></awc-panel>
+    </awc-tabs>
+  `;
+
+  it('should initialise its markup correctly', async () => {
+    const el = await fixture(markup);
+
+    await elementUpdated(el);
+
+    expect(el).dom.to.equal(`
+      <awc-tabs role="tablist">
+        <awc-tab
+          aria-controls="awc-panel-generated-1"
+          id="awc-tab-generated-1"
+          role="tab"
+          selected=""
+          slot="tab"
+          tabindex="0">
+        </awc-tab>
+        <awc-panel
+          aria-labelledby="awc-tab-generated-1"
+          id="awc-panel-generated-1"
+          role="tabpanel"
+          slot="panel">
+        </awc-panel>
+      </awc-tabs>
+    `);
+  });
+
+  it('has a static shadow-dom', async () => {
+    const el = await fixture(html` <awc-tabs></awc-tabs> `);
+
+    expect(el).shadowDom.to.equal(`
+      <slot name="tab"></slot>
+      <slot name="panel"></slot>
+    `);
+  });
+
+  it('should pass an automated accessibility test', async () => {
+    const el = await fixture(markup);
+
+    await elementUpdated(el);
+
+    expect(el).to.be.accessible();
+  });
+});
